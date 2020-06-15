17d18066b9b959568f4f5ba8a7ece309229e354f
yoonwoojae <dbsdnwp1004@naver.com>
2020-06-15 10:39:39 +0900

    add redux

--- lib/pages/products_page.dart
+++ lib/pages/products_page.dart
@@ -1,40 +1,30 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/models/app_state.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ProductsPage extends StatefulWidget {
+  final void Function() onInit;
+  ProductsPage({this.onInit});
+
   @override
   ProductsPageState createState() => ProductsPageState();
 }
 
 class ProductsPageState extends State<ProductsPage> {
-  @override
   void initState() {
     super.initState();
-    _getUser();
-  }
-
-  _getUser() async {
-    final prefs = await SharedPreferences.getInstance();
-    var storeUser = prefs.getString('user');
-    print(json.decode(storeUser));
+    widget.onInit();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('products'),
-        leading: Icon(Icons.email),
-        actions: <Widget>[
-          Icon(Icons.face),
-          Icon(Icons.add),
-        ],
-      ),
-      body: Center(
-        child: Text('products'),
-      ),
-    );
+    return StoreConnector<AppState, AppState>(
+        converter: (store) => store.state,
+        builder: (context, state) {
+          return Text(json.encode(state.user));
+        });
   }
 }
